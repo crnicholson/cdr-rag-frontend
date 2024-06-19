8a96f30dc4ee84b9201bff7f18a7a5dfedc957f4
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-19 07:51:21 -0400

    More UI work

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,7 @@ export default function Home() {
     });
 
     if (response.ok) {
+      setChatbotResponse("Thinking...");
       const data = await response.json();
       setChatbotResponse(data.message);
       console.log('Message sent successfully!');
@@ -44,24 +45,25 @@ export default function Home() {
 
   return (
     <>
-      <div className="min-h-screen flex flex-col bg-[#f8f6ec] text-[#606c38]">
+      <div className="min-h-screen flex flex-col bg-[#f8f6ec] text-[#464f28]">
         <div className="flex flex-col items-center flex-grow">
           <div className="p-10 w-[80%]">
             <h1 className="text-3xl font-bold">Carbon Dioxide Removal (CDR) Chatbot</h1>
             <div className="flex flex-row items-center mt-5">
-              <a href="/about" className="underline decoration-dotted mr-2">About</a>
+              <a href="/about" className="underline decoration-dotted mr-2 hover:text-[#606c38ea] ">About</a>
               <p className="mr-2">|</p>
-              <a href="/quickstart" className="underline decoration-dotted mr-2">Quickstart</a>
+              <a href="/quickstart" className="hover:text-[#606c38ea] underline decoration-dotted mr-2">Quickstart</a>
             </div>
             <p className="mt-5">Enter your ChatGPT API key below to start chatting with hundreds of recent, curated CDR papers. Don't worry, we won't steal your API key.</p>
-            <p><b>Note:</b> this does use 150 tokens. You get 1 million tokens for $2.</p>
+            <p className="mt-2"><b>Note:</b> this does use 150 tokens. You get 1 million tokens for $2, so about 0.03 cents per question.</p>
+            <p className=""><b>Note:</b> this is a dev preview. Security issues may occur. Do not put more than $1 on your API key.</p>
             <div className="flex flex-row my-5 items-center">
-              <textarea className="h-min p-1 mr-5 border-black border-[1px] rounded-lg bg-gray-100" onChange={e => key = e.target.value} placeholder="API Key" />
-              <button className="p-1 h-10 border-black border-dotted border-[2px] hover:bg-gray-200 rounded-lg bg-[#d1d6c7c6]" onClick={() => updateKey(key)}>Submit</button>
+              <textarea className="h-min p-1 mr-5 border-[#464f28] border-[1px] rounded-lg bg-gray-100" onChange={e => key = e.target.value} placeholder="API Key" />
+              <button className="p-1 h-10 border-[#464f28] border-dotted border-[2px] hover:bg-[#c0c6aaea] rounded-lg bg-[#d1d6c7c6]" onClick={() => updateKey(key)}>Submit</button>
             </div>
             <p>Enter your prompt:</p>
-            <textarea className="p-1 h-[150px] mr-5 my-5 border-black border-[1px] rounded-lg bg-gray-100 w-full" onChange={e => message = e.target.value} placeholder="Tell me about recent CDR trends." />
-            <button className="h-10 p-1 border-black border-[2px] hover:bg-gray-200 rounded-lg bg-[#d1d6c7c6] border-dotted" onClick={() => sendMessage(message)}>Submit</button>
+            <textarea className="p-1 h-[150px] mr-5 my-5 border-[#464f28] border-[1px] rounded-lg bg-gray-100 w-full" onChange={e => message = e.target.value} placeholder="Tell me about recent CDR trends." />
+            <button className="h-10 p-1 border-[#464f28] border-[2px] hover:bg-[#c0c6aaea] rounded-lg bg-[#d1d6c7c6] border-dotted" onClick={() => sendMessage(message)}>Submit</button>
             <p className="mt-5">Response:</p>
             <p className="mt-5 p-1 border-black border-[1px] rounded-lg bg-gray-100">{chatbotResponse}</p>
           </div>
