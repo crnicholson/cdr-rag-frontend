c923ed058f6bf70561328f4966725db5e433b709
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-06-19 15:08:46 -0400

    Small changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,7 @@ export default function Home() {
   const [chatbotResponse, setChatbotResponse] = useState("Enter a prompt and submit first!");
   const [message, setMessage] = useState('');
   const [key, setKey] = useState('');
-  const [keyVisibility, setKeyVisibility] = useState("visible");
-  const [keyVisibilityText, setKeyVisibilityText] = useState("Hide");
-
-  // const updateKey = async (message: string) => {
-  //   const response = await fetch('https://cdr.cnicholson.hackclub.app/api/send-key', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ message }),
-  //   });
-
-  //   if (response.ok) {
-  //     console.log('API key sent successfully!');
-  //   } else {
-  //     console.error('Error sending API key:', response.statusText);
-  //   }
-  // }
+  const [isBlurred, setIsBlurred] = useState(false);
 
   const sendMessage = async (message: string) => {
     const response = await fetch('https://cdr.cnicholson.hackclub.app/api/send-message', {
@@ -58,8 +41,8 @@ export default function Home() {
             <p className="mt-5">Enter your ChatGPT API key below to start chatting with hundreds of recent, curated CDR papers. Don{"'"}t worry, we won{"'"}t steal your API key. However, this does use 150 tokens. You get 1 million tokens for $2, so about 0.03 cents per question.</p>
             <p className=""><b>Note:</b> this is a dev preview. Security issues may occur. Do not put more than $1 on your API key.</p>
             <div className="flex flex-row my-5 items-center">
-              <textarea className={"h-min p-1 mr-5 border-[#464f28] border-[1px] rounded-lg bg-gray-100" + keyVisibility} onChange={e => setKey(e.target.value)} placeholder="API Key"/>
-              <button className="p-1 h-10 border-[#464f28] border-dotted border-[2px] hover:bg-[#c0c6aaea] rounded-lg bg-[#d1d6c7c6]" onClick={() => { setKeyVisibility("invisible"), setKeyVisibilityText("Show")}}>{keyVisibilityText}</button>
+              <textarea className="h-min p-1 mr-5 border-[#464f28] border-[1px] rounded-lg bg-gray-100" onChange={e => setKey(e.target.value)} placeholder="API Key"/>
+              <button className="p-1 h-10 border-[#464f28] border-dotted border-[2px] hover:bg-[#c0c6aaea] rounded-lg bg-[#d1d6c7c6]">Show</button>
             </div>
             <p>Enter your prompt:</p>
             <textarea className="p-1 h-[150px] mr-5 my-5 border-[#464f28] border-[1px] rounded-lg bg-gray-100 w-full" value={message} onChange={e => setMessage(e.target.value)} placeholder="Tell me about recent CDR trends." />
